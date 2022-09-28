94da78cec1f81fcaa8f8d3c1bd07cfb6e23cb8fb
SebastianSota <74997686+SebastianSota@users.noreply.github.com>
2022-09-27 23:11:48 -0500

    first commit

---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', (event) => {
+    // console.log("SW: install");
+    // const promiseCache = caches.open('cache-v1.1').then((cache) =>{
+    //     return cache.addAll(
+    //         [
+    //             '/',
+    //             '/index.html',
+    //             '/css/style.css',
+    //             '/js/app.js',
+    //             '/images/Esfera_cristal.jpeg',
+    //             'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+    //             'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+    //         ]
+    //     );
+    // });
+
+    const promiseCache = caches.open('imported-elements').then((cache) =>{
+        return cache.addAll(
+            [
+                '/',
+                '/index.html',
+                '/pages/dividir.html',
+                '/pages/multiplicar.html',
+                '/pages/restar.html',
+                '/pages/sumar.html',
+                '/js/app.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+            ]
+        )
+    });
+
+    event.waitUntil(promiseCache);
+});
+
+//Only cache
+self.addEventListener('fetch', (event) =>{
+    const respCache = caches.match(event.request)
+    event.respondWith(respCache);
+});
+
